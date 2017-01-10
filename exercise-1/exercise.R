ebb7809e4858ca5973d9bd568be6fd20baccafb6
David Lee <david168@uw.edu>
2017-01-10 09:38:05 -0800

    finish exercises 1, 2, 3

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -3,14 +3,38 @@
 # Define a function `AddThree` that takes a single argument and
 # returns a value 3 greater than that input
 
+number <- 7
 
+addThree <- function(number) {
+  ten <- number + 3
+  
 # Create a variable `ten` that is the result of passing 7 to your `AddThree` function
 
+  return(ten)
+}
+
+print(ten)
 
 # Define a function `ImperialToMetric` that takes in two arguments: a number of feet
 # and a number of inches
 # The function should return the total length in meters
 
+feet <- 5
+inches <- 9
 
+ImperialToMetric <- function(feet, inches) {
+  inches <- inches + feetToInches(feet)
+  height.in.meters <- inches * 2.54
+  
+  return(height.in.meters)
+}
+
+feetToInches <- function(feet) {
+  feetConvertedToInches <- feet * 12
+  
+  return(feetConvertedToInches)
+}
+
+print(height.in.meters)
 # Create a variable `height.in.meters` by passing your height in imperial to the
 # `ImperialToMetric` function
--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -3,14 +3,31 @@
 # Define a function `CompareLength` that takes in 2 character strings, and returns
 # the sentence of the form
 #   "The difference in length is N"
-
-
 # Pass two strings of different lengths to your `CompareLength` function
 
+compareLength <- function(string1, string2) {
+  characterDifference <- abs(nchar(string1) - nchar(string2))
+  result <- paste("The difference in length is: ", characterDifference)
+  return(result)
+}
+
+print(compareLength("dog","book"))
 
 # Define a function `DescribeDifference` that will return one of the following statements:
-# "Your first string is longer by N characters"
-# "Your second string is longer by N characters"
-
-
+# Your first string is longer by N characters
+# Your second string is longer by N characters
 # Pass two strings of different lengths to your `DescribeDifference` function
+
+DescribeDifference <- function(String1, String2) {
+  characterDifference <- nchar(String1) - nchar(String2)
+  if (characterDifference > 0) {
+    result <- paste("String 1 is longer by ", characterDifference, "characters!")
+  } else if (characterDifference < 0) {
+    result <- paste("String 2 is longer by ", abs(characterDifference), "characters!")
+  } else {
+    result <- "Both String 1 and String 2 are equal length!"
+  }
+  return(result)
+}
+
+print(DescribeDifference("dog", "dog"))
